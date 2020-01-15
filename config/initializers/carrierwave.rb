d38561595cf84fcb30441ed60143caf7d71dc772
Oana Blankenship <ancutadraguta20@gmail.com>
2020-01-15 03:08:52 +0000

    carrierwave.rb update for AWS testing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl    = "public-read"
+  config.aws_acl    = "public_read"
 
   config.aws_credentials = {
       access_key_id:     ENV["AWS_ACCESS_KEY"],
